8c27d4123f9693bfd42b3bd9f5f643b08e561ad2
David Sanders <dsanders@rapilabs.com>
2015-08-26 22:03:54 +1000

    Use with statement

--- local/import_data.py
+++ local/import_data.py
@@ -26,37 +26,36 @@ def import_data(path, Admin, Location):
             model = Location
             fields = '__all__'
 
-    fin = open(path, 'rb')
-    xin = csv.DictReader(fin, delimiter='\t', fieldnames=field_names)
+    with open(path, 'rb') as fin:
+        xin = csv.DictReader(fin, delimiter='\t', fieldnames=field_names)
 
-    for count, row in enumerate(xin):
-        # Extract and find Admin 1, 2, 3
-        if row['admin_code1']:
-            rec, created = Admin.objects.get_or_create(
-                code=row['admin_code1'],
-                name=row['admin_name1'],
-                group=1
-            )
-            row['admin1'] = rec.id
-        if row['admin_code2']:
-            rec, created = Admin.objects.get_or_create(
-                code=row['admin_code2'],
-                name=row['admin_name2'],
-                group=2
-            )
-            row['admin2'] = rec.id
-        if row['admin_code3']:
-            rec, created = Admin.objects.get_or_create(
-                code=row['admin_code3'],
-                name=row['admin_name3'],
-                group=3
-            )
-            row['admin3'] = rec.id
+        for count, row in enumerate(xin):
+            # Extract and find Admin 1, 2, 3
+            if row['admin_code1']:
+                rec, created = Admin.objects.get_or_create(
+                    code=row['admin_code1'],
+                    name=row['admin_name1'],
+                    group=1
+                )
+                row['admin1'] = rec.id
+            if row['admin_code2']:
+                rec, created = Admin.objects.get_or_create(
+                    code=row['admin_code2'],
+                    name=row['admin_name2'],
+                    group=2
+                )
+                row['admin2'] = rec.id
+            if row['admin_code3']:
+                rec, created = Admin.objects.get_or_create(
+                    code=row['admin_code3'],
+                    name=row['admin_name3'],
+                    group=3
+                )
+                row['admin3'] = rec.id
 
-        row['location'] = 'POINT(%(longitude)s %(latitude)s)' % row
-        form = ImportForm(row)
-        if form.is_valid():
-            form.save()
-        else:
-            print "[%s] Failed: %s\n" % (count, form.errors,)
-    fin.close()
+            row['location'] = 'POINT(%(longitude)s %(latitude)s)' % row
+            form = ImportForm(row)
+            if form.is_valid():
+                form.save()
+            else:
+                print "[%s] Failed: %s\n" % (count, form.errors,)
